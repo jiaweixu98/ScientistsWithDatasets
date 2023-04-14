80d3bc442ab8f08c3f3d55cae4e06416a1e6875e
Xu Jiawei（许家伟） <60133171+jiaweixu98@users.noreply.github.com>
2023-04-14 16:18:06 +0800

    upload the graph

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
+import plotly.express as px
 import numpy as np
 import pandas as pd
 import textwrap # 用来给悬浮窗自动换行
@@ -16,7 +17,12 @@ dataset = pd.read_csv('data/dataset.csv', index_col=0)
 author = pd.read_csv('data/author.csv', index_col=0)
 B2AIauthor = author.loc[author['isB2AI'] == 1]
 
-
+GreatGPT = ['Bioentities: Cancer Signaling Molecules<br>Keywords: Therapeutic Mechanisms', 'Bioentities: Health & Disease Risk Factors<br>Keywords: Epidemiology & Research Methods',
+'Bioentities: Breast Cancer Genetics Biomarkers<br>Keywords: Genetics & Biomarkers', 
+'Bioentities: Breast Cancer Treatments<br>Keywords: Cancer Treatment & Management',
+'Bioentities: Tumor Microenvironment Metastasis Molecules<br>Keywords: Tumor Metastasis & Matrix Biology']
+ClusterPosition = [[-10,50],[55,-50],[-40,-50],[75,20],[-75,20],]
+# Clusteranlge = [0,-45,0,45,-60]
 # 开始画图，构建一个Figure()
 figSwD = go.Figure()
 # 5个聚类的trace先画出来，C0author只是个名字，代表这一类的作者
@@ -32,7 +38,7 @@ for i in range(5):
         # <extra></extra> 是为了隐藏第二个框框，不加这行代码就很丑
                               marker=dict(size=3),# 设置了较小的值，代表一般作者的大小
                             # this is the name of the trace
-                            name='Scientists Cluster: %s'%i,
+                            name=GreatGPT[i],
                             ))
 # 此处是dataset的trace
 figSwD.add_trace(go.Scattergl(x=dataset['X'], y=dataset['Y'],
@@ -42,7 +48,7 @@ figSwD.add_trace(go.Scattergl(x=dataset['X'], y=dataset['Y'],
     customdata=np.stack((dataset.index, dataset['Name']), axis=-1),
     mode='markers',
     hovertemplate='<br><br><b>Dataset ID<b>: %{customdata[0]}<br><b>Dataset Name<b>: %{customdata[1]}<extra></extra>',
-    name='Data Sets',
+    name='<b>'+'DataSets'+'<b>',
    
 ))
 # B2AIauthor，breast cancer area只有六个人
@@ -54,11 +60,34 @@ figSwD.add_trace(go.Scattergl(x=B2AIauthor['X'], y=B2AIauthor['Y'],
                               marker=dict(size=30),
                               marker_symbol=22,
                               # this is the name of the trace
-                              name='Bridge2AI Talent in Breast Cancer',
+                              name='<b>'+'Bridge2AI Talent in Breast Cancer' + '<b>',
                               ))
+for i in range(5):
+    figSwD.add_annotation(
+    x=ClusterPosition[i][0],
+    y=ClusterPosition[i][1],
+    xref="x",
+    yref="y",
+    text='<b>'+GreatGPT[i]+'<b>',
+        showarrow=False,
+    font=dict(
+        size=10,
+        color=px.colors.qualitative.Plotly[i]
+    ),
+    align="center",
+        bordercolor="#FFFFFF",
+        borderwidth=2,
+        borderpad=4,
+        bgcolor="#FFFFFF",
+    opacity=0.95,
+        # textangle=Clusteranlge[i]
+)
 
 # 设置下整体的layout
 figSwD.update_layout(
+    legend=dict(itemsizing='constant',title_font_family="Times New Roman",
+                              font=dict(size= 12)),
+    dragmode='pan',
     hoverlabel_align = 'left',
     hoverlabel=dict(
         font_size=10,
@@ -105,7 +134,8 @@ app.layout = html.Div(
                     children=dcc.Graph(
                         id="MDS", # ID随便起的，似乎不重要
                         figure=figSwD,# 这里很重要，是上边画的图
-                        config={'scrollZoom':True} # 鼠标滚轴可以缩放，默认关闭，这里打开
+                        config={'scrollZoom': True,
+                                }  # 鼠标滚轴可以缩放，默认关闭，这里打开
                     ),
                     className="card",
                 ),
